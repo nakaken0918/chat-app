7257a4a1d5cc0e7c340c2cb724f6899161aa9b88
nakaken0918 <nakaken18.wywy@gmail.com>
2023-05-24 18:37:16 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  #ログインしてないユーザーをログインページに飛ばす。
+  before_action :authenticate_user!
+  #全てのアクション実行の前にワンクッション設ける。セキュリティ対策。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    #deviseでユーザー登録するとき、以下のメソッドで設定したカラムが許可される。
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
