e7d98c01800b76b229a0fc1af40a7faec462f087
nakaken0918 <nakaken18.wywy@gmail.com>
2023-05-25 18:03:27 +0900

    room_usersテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,7 @@
 class Room < ApplicationRecord
+  #room_userと１：多の関係にあることを示す。
+  has_many :room_users
+  #room_userを介してuserテーブルと１：多の関係になることを示す。
+  has_many :users, through: :room_users
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   #バリデーションの設定。nameのフォームが入力されていること。
   validates :name, presence: true
+  #room_userと１：多の関係にあることを示す。
+  has_many :room_users
+  #room_userを介してroomテーブルと１：多の関係になることを示す。
+  has_many :rooms, through: :room_users
 
 end
