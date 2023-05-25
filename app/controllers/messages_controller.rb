99915d54c84e176329aed78dca86afd2ae3e3844
nakaken0918 <nakaken18.wywy@gmail.com>
2023-05-25 21:41:08 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,16 +5,19 @@ class MessagesController < ApplicationController
     #paramsに含まれているroom_idを代入。
     @room = Room.find(params[:room_id])
 
+    @messages = @room.messages.includes(:user)
+
   end
 
   def create
 
     @room = Room.find(params[:room_id])
-    
+
     @message = @room.messages.new(message_params)
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :index
     end
 
