5120e2bc67ae5046cdafe43cb9ff71d6d32d661d
nakaken0918 <nakaken18.wywy@gmail.com>
2023-05-25 20:01:07 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,5 +3,25 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+  #createアクションを定義する。
+  
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      # 保存できたらルートパスへ。
+      redirect_to root_path
+    else
+      # 保存できなかったらrooms/new.html.erbへ戻る。
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    # 送られてくる値が配列である場合、キーに対して[]を値として記述する。
+    params.require(:room).permit(:name, user_ids: [])
+
+  end
 
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -3,5 +3,7 @@ class Room < ApplicationRecord
   has_many :room_users
   #room_userを介してuserテーブルと１：多の関係になることを示す。
   has_many :users, through: :room_users
+  #バリデーションを設定。ルーム名が入力されている（presence）場合に許可する。
+  validates :name, presence: true
 
 end
