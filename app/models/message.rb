3873cdb7e8d7e7d0e92a1da37a52715d191dda1f
nakaken0918 <nakaken18.wywy@gmail.com>
2023-05-26 16:53:45 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,8 @@ class Message < ApplicationRecord
   belongs_to :room
   #userテーブルと多：１の関係であることを示す。
   belongs_to :user
+  #画像ファイルのテーブルと１：１の関係であることを示す。
+  has_one_attached :image
   #contentカラムに情報が入力されていれば許可される。
   validates :content, presence: true
 
--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -2,9 +2,9 @@ class RoomUser < ApplicationRecord
   #roomとuserの組み合わせを管理する中間テーブルを設定する。
   #roomとuserのデータが欲しい時、それぞれ別で探すより組み合わせから探す方が無駄が少ない。
 
-  #roomテーブルと多：１の関係にあることを示す。
+  #roomテーブルと多：１の関係にあることを示す。※roomテーブルに所属することを示す。
   belongs_to :room
-  #userテーブルと多：１の関係にあることを示す。
+  #userテーブルと多：１の関係にあることを示す。※userテーブルに所属することを示す。
   belongs_to :user
 
 end
