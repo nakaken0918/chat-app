88fa614ded7a11630be96d47b03d8d3201b3cd75
nakaken0918 <nakaken18.wywy@gmail.com>
2023-05-26 17:12:39 +0900

    画像だけでも送信できるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,6 +6,10 @@ class Message < ApplicationRecord
   #画像ファイルのテーブルと１：１の関係であることを示す。
   has_one_attached :image
   #contentカラムに情報が入力されていれば許可される。
-  validates :content, presence: true
+  validates :content, presence: true, unless: :was_attached?
+  #画像ファイルの有無をチェックする。true か falseで unlessに返答する。
+  def was_attached?
+    self.image.attached?
+  end
 
 end
